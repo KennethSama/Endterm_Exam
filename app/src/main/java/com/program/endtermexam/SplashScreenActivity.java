69c62715b9b0e5abdb6440718d111f49f3d42061
kenjie-sama <86190550+kenjie-sama@users.noreply.github.com>
2021-12-11 17:56:45 -0800

    Splashscreen    Created Splashscreen with vector assets    Edited themes to no actionbar    Changed strings.xml app_name to Pacefy    ic_curves1.xml    ic_ic

---
+++ app/src/main/java/com/program/endtermexam/SplashScreenActivity.java
@@ -0,0 +1,21 @@
+package com.program.endtermexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Handler _handler = new Handler();
+        _handler.postDelayed(() -> {
+            Intent intent_homeScreen = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent_homeScreen);
+            finish();
+        }, 4000);
+    }
+}
