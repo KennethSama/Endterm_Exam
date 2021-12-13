d492531685f4cf59a394c02c64895235c8a6fcf4
kenjie-sama <86190550+kenjie-sama@users.noreply.github.com>
2021-12-12 16:18:24 -0800

    Dashboard Created     Added SGV icons for quizzes, attendance and grades     Added Custom actionbar

--- app/src/main/java/com/program/endtermexam/LoginMenu.java
+++ app/src/main/java/com/program/endtermexam/LoginMenu.java
@@ -8,15 +8,24 @@ import android.view.View;
 
 public class LoginMenu extends AppCompatActivity {
     private Intent intent_signup;
+    private Intent intent_dashboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_menu);
         InitializeIntents();
     }
-    private void InitializeIntents(){ intent_signup = new Intent(LoginMenu.this, SignupMenu.class); }
+    private void InitializeIntents(){
+        intent_signup = new Intent(LoginMenu.this, SignupMenu.class);
+        intent_dashboard = new Intent(LoginMenu.this, Dashboard.class);
+    }
     public void OnSignUp(View view) {
         startActivity(intent_signup);
+        finish();
+    }
+
+    public void OnDashboard(View view) {
+        startActivity(intent_dashboard);
         finish();
     }
 }
--- app/src/main/java/com/program/endtermexam/SignupMenu.java
+++ app/src/main/java/com/program/endtermexam/SignupMenu.java
@@ -7,15 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 
 public class SignupMenu extends AppCompatActivity {
+    private Intent intent_login;
+    private Intent intent_dashboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_menu);
+        InitializeIntents();
+    }
+    private void InitializeIntents(){
+        intent_login = new Intent(SignupMenu.this, LoginMenu.class);
+        intent_dashboard = new Intent(SignupMenu.this, Dashboard.class);
     }
 
     public void OnLogin(View view) {
-        Intent intent_login = new Intent(SignupMenu.this, LoginMenu.class);
         startActivity(intent_login);
+        finish();
+    }
+
+    public void OnDashboard(View view) {
+        startActivity(intent_dashboard);
         finish();
     }
 }
