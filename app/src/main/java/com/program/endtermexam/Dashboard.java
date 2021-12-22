baf4c25b216d2f7761b14eacca498e150effd913
kenjie-sama <86190550+kenjie-sama@users.noreply.github.com>
2021-12-22 00:33:37 -0800

    Added ViewTopic, ViewCourse Templates Added items in strings.xml Improved Dashboard and Courses Layout Ignore Error and Warnings (Errors are caused by the duplication of IDs)

--- app/src/main/java/com/program/endtermexam/Dashboard.java
+++ app/src/main/java/com/program/endtermexam/Dashboard.java
@@ -2,13 +2,27 @@ package com.program.endtermexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
-
+    private Intent intent_viewAll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        InitializeIntents();
+    }
+    private void InitializeIntents(){
+        intent_viewAll = new Intent(Dashboard.this, courses.class);
+    }
+    public void ViewAll(View view) {
+        Toast.makeText(this, String.format("View ID: %s", view.getId()), Toast.LENGTH_SHORT).show();
+        startActivity(intent_viewAll);
+//        Log.d("View All ID", String.format("View ID: %s", view.getId()));
     }
 }
---
+++ app/src/main/java/com/program/endtermexam/ViewTopic.java
@@ -0,0 +1,14 @@
+package com.program.endtermexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewTopic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_topic);
+    }
+}
